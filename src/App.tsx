4f289e5f2a6f7fa326e944e098a9a46d9c62f83c
Oskari Lehto <oskari.lehto@gmail.com>
2021-09-06 18:38:43 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,587 @@
+import React from 'react';
+import './App.scss';
+
+const range = (min: number, max: number) => {
+  return [...Array(max - min + 1).keys()].map(i => i + min)
+}
+
+const colorRoad = '#b1aaa1'
+const colorCastle = '#7a5233'
+const colorCastleBoarder = '#6b482d'
+const colorWater = '#2358a7'
+const colorMonastery = '#96582f'
+const colorMonasteryBorder = '#704122'
+const colorGround = '#459926'
+const colorGroundBorder = '#3e8a22'
+
+enum PieceSideType {
+  empty,
+  road,
+  castle,
+  river,
+}
+
+enum PieceExtraInfo {
+  empty,
+  oppositeCastleFull,
+  monastery,
+  nonConnectedSideBySideCastle,
+}
+
+class Piece {
+  // bottom, left, top, right
+  sideTypes: [PieceSideType, PieceSideType, PieceSideType, PieceSideType];
+  extraInfo: PieceExtraInfo
+
+  constructor(
+    sideTypes: [PieceSideType, PieceSideType, PieceSideType, PieceSideType],
+    extraInfo: PieceExtraInfo = PieceExtraInfo.empty,
+  ) {
+    this.sideTypes = sideTypes
+    this.extraInfo = extraInfo
+  }
+
+  getBottom() {
+    return this.sideTypes[0]
+  }
+
+  getLeft() {
+    return this.sideTypes[1]
+  }
+
+  getTop() {
+    return this.sideTypes[2]
+  }
+
+  getRight() {
+    return this.sideTypes[3]
+  }
+
+  getImageDataUrl() {
+    const canvas = document.createElement('canvas')
+    canvas.width = 100
+    canvas.height = 100
+    const ctx = canvas.getContext('2d')!
+
+    // Full castle
+    if (this.sideTypes.every((s) => s === PieceSideType.castle)) {
+      ctx.fillStyle = colorCastle
+      ctx.fillRect(0, 0, 100, 100)
+      ctx.fill()
+      ctx.stroke()
+      return canvas.toDataURL("image/png");
+    }
+
+    ctx.lineWidth = 5;
+
+    // Fill groun with green
+    ctx.fillStyle = colorGround
+    ctx.strokeStyle = colorGroundBorder
+    ctx.fillRect(0, 0, 100, 100)
+    ctx.strokeRect(0, 0, 100, 100)
+
+    for (let i = 0; i < 4; i++) {
+      const sideType = this.sideTypes[i]
+      ctx.translate(50, 50)
+
+      // Roads and rivers
+      if (sideType === PieceSideType.road || sideType === PieceSideType.river) {
+        if (sideType === PieceSideType.river) {
+          ctx.strokeStyle = colorWater
+        } else {
+          ctx.strokeStyle = colorRoad
+        }
+
+        const canArch = (
+          !(this.extraInfo === PieceExtraInfo.monastery)
+        )
+        if (
+          canArch &&
+          sideType === this.sideTypes[(i + 4 - 1) % 4] &&
+          sideType !== this.sideTypes[(i + 1) % 4] &&
+          sideType !== this.sideTypes[(i + 4 - 2) % 4]
+        ) {
+          // Do not draw corners twice
+        } else if (
+          canArch &&
+          sideType === this.sideTypes[(i + 1) % 4] &&
+          sideType !== this.sideTypes[(i + 2) % 4] &&
+          sideType !== this.sideTypes[(i + 3) % 4]
+        ) {
+          // Corners
+          ctx.beginPath();
+          ctx.arc(-50, 50, 50, 0, 2 * Math.PI);
+          ctx.stroke();
+        } else {
+          if (
+            sideType === PieceSideType.river && (
+              this.sideTypes.some(s => s === PieceSideType.road) ||
+              this.extraInfo === PieceExtraInfo.monastery
+            )
+          ) {
+            // Water should bend to avoid roads or monastery
+            const thing = Math.floor(i / 2) % 2 === 0 ? 15 : -15; // Needed to make water go around on the same side on both directions
+
+            ctx.beginPath();
+            ctx.moveTo(thing, 0);
+            ctx.lineTo(thing, 15);
+            ctx.lineTo(0, 30);
+            ctx.lineTo(0, 50);
+            ctx.stroke();
+          } else {
+            // Regular stuff
+
+            // TODO: Handle going into castles correctly (do not draw inside castles)
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, 50);
+            ctx.stroke();
+          }
+        }
+
+        // Draw villages and river endings
+        const roadOrRiverCount = this.sideTypes.reduce((a: number, s) => a + (s === sideType ? 1 : 0), 0)
+
+        if (roadOrRiverCount === 1 && sideType === PieceSideType.river) {
+          ctx.strokeStyle = colorWater
+          ctx.fillStyle = colorWater
+
+          ctx.beginPath();
+          ctx.arc(0, 0, 10, 0, 2 * Math.PI);
+          ctx.fill();
+          ctx.stroke();
+        }
+        if (
+          roadOrRiverCount !== 2 &&
+          sideType === PieceSideType.road
+        ) {
+          // TODO: Do not draw this inside castles
+          ctx.strokeStyle = colorRoad
+          ctx.fillStyle = colorRoad
+
+          ctx.beginPath();
+          ctx.moveTo(-10, -10);
+          ctx.lineTo(10, -10);
+          ctx.lineTo(10, 10);
+          ctx.lineTo(-10, 10);
+          ctx.lineTo(-10, -10);
+          ctx.fill()
+        }
+      }
+
+      // Castles
+      if (
+        sideType === PieceSideType.castle
+        && (
+          this.sideTypes[(i + 4 - 1) % 4] !== PieceSideType.castle || // Drawing handled in another side (only draw corners once)
+          this.extraInfo === PieceExtraInfo.nonConnectedSideBySideCastle // Except when non connected corner case...
+        )
+      ) {
+        ctx.fillStyle = colorCastle
+        ctx.strokeStyle = colorCastleBoarder
+
+        if (
+          this.sideTypes[(i + 1) % 4] === PieceSideType.castle &&
+          this.sideTypes[(i + 2) % 4] === PieceSideType.castle
+        ) {
+          // Handle 3 sided castle
+          ctx.beginPath();
+          ctx.moveTo(50, -50);
+          ctx.lineTo(-25, -25);
+          ctx.lineTo(-25, 25);
+          ctx.lineTo(50, 50);
+          ctx.lineTo(-50, 50);
+          ctx.lineTo(-50, -50);
+          ctx.fill();
+
+          ctx.beginPath();
+          ctx.moveTo(50, -50);
+          ctx.lineTo(-25, -25);
+          ctx.lineTo(-25, 25);
+          ctx.lineTo(50, 50);
+          ctx.stroke()
+        } else if (
+          this.sideTypes[(i + 1) % 4] === PieceSideType.castle &&
+          this.extraInfo !== PieceExtraInfo.nonConnectedSideBySideCastle
+        ) {
+          // Handle 2 sided castle
+          ctx.beginPath();
+          ctx.moveTo(-50, -50);
+          ctx.lineTo(-10, 10);
+          ctx.lineTo(50, 50);
+          ctx.lineTo(-50, 50);
+          ctx.fill();
+          ctx.beginPath()
+          ctx.moveTo(-50, -50);
+          ctx.lineTo(-10, 10);
+          ctx.lineTo(50, 50);
+          ctx.stroke();
+        } else {
+          // Handle single castle
+
+          if (
+            this.sideTypes[(i + 2) % 4] === PieceSideType.castle &&
+            this.extraInfo === PieceExtraInfo.oppositeCastleFull
+          ) {
+            // Handle connected opposite castle
+            // Only draw connected opposite castle once
+            if (i === 0 || i === 1) {
+              ctx.beginPath();
+              ctx.moveTo(-50, -50);
+              ctx.lineTo(-20, -20);
+              ctx.lineTo(-20, 20);
+              ctx.lineTo(-50, 50);
+              ctx.lineTo(50, 50);
+              ctx.lineTo(20, 20);
+              ctx.lineTo(20, -20);
+              ctx.lineTo(50, -50);
+              ctx.fill();
+
+              ctx.beginPath()
+              ctx.moveTo(-50, -50);
+              ctx.lineTo(-20, -20);
+              ctx.lineTo(-20, 20);
+              ctx.lineTo(-50, 50);
+              ctx.stroke()
+
+              ctx.beginPath()
+              ctx.lineTo(50, 50);
+              ctx.lineTo(20, 20);
+              ctx.lineTo(20, -20);
+              ctx.lineTo(50, -50);
+              ctx.stroke()
+            }
+          } else {
+            // Regular 1 sided castle
+            ctx.lineWidth = 5;
+            ctx.beginPath();
+            ctx.arc(0, 105, 75, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+          }
+        }
+      }
+
+      ctx.rotate(Math.PI / 2)
+      ctx.translate(-50, -50)
+    }
+
+    // Monastery
+    if (this.extraInfo === PieceExtraInfo.monastery) {
+      ctx.fillStyle = colorMonastery
+      ctx.strokeStyle = colorMonasteryBorder
+      ctx.beginPath();
+      ctx.moveTo(40, 40);
+      ctx.lineTo(50, 30);
+      ctx.lineTo(60, 40);
+      ctx.lineTo(60, 60);
+      ctx.lineTo(40, 60);
+      ctx.lineTo(40, 40);
+      ctx.fill()
+      ctx.stroke();
+    }
+
+    return canvas.toDataURL("image/png");
+  }
+
+  getRotated(rotation: number) {
+    return new Piece(
+      [
+        this.sideTypes[(0 + rotation) % 4],
+        this.sideTypes[(1 + rotation) % 4],
+        this.sideTypes[(2 + rotation) % 4],
+        this.sideTypes[(3 + rotation) % 4],
+      ],
+      this.extraInfo
+    )
+  }
+
+  getHash() {
+    return `${this.sideTypes[0]}-${this.sideTypes[1]}-${this.sideTypes[2]}-${this.sideTypes[3]}-${this.extraInfo}`
+  }
+}
+
+const pieces = [
+  new Piece([
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.road,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.road,
+    PieceSideType.empty,
+    PieceSideType.road,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.empty,
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.castle,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.castle,
+  ], PieceExtraInfo.nonConnectedSideBySideCastle),
+  new Piece([
+    PieceSideType.empty,
+    PieceSideType.road,
+    PieceSideType.castle,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.road,
+    PieceSideType.castle,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.empty,
+    PieceSideType.castle,
+    PieceSideType.empty,
+  ], PieceExtraInfo.oppositeCastleFull),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.road,
+    PieceSideType.castle,
+    PieceSideType.empty,
+  ], PieceExtraInfo.oppositeCastleFull),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.castle,
+    PieceSideType.castle,
+  ]),
+  new Piece([
+    PieceSideType.castle,
+    PieceSideType.road,
+    PieceSideType.road,
+    PieceSideType.empty,
+  ], PieceExtraInfo.monastery),
+  new Piece([
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ], PieceExtraInfo.monastery),
+  new Piece([
+    PieceSideType.road,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ], PieceExtraInfo.monastery),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.empty,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.empty,
+    PieceSideType.river,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.river,
+    PieceSideType.empty,
+    PieceSideType.empty,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.empty,
+    PieceSideType.river,
+    PieceSideType.road,
+  ], PieceExtraInfo.monastery),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.empty,
+    PieceSideType.river,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.river,
+    PieceSideType.road,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.castle,
+    PieceSideType.river,
+    PieceSideType.road,
+  ]),
+  new Piece([
+    PieceSideType.river,
+    PieceSideType.river,
+    PieceSideType.castle,
+    PieceSideType.castle,
+  ]),
+]
+
+const allRotatedPieces = pieces.reduce((a: Piece[], piece) => {
+  return [
+    ...a,
+    piece,
+    piece.getRotated(1),
+    piece.getRotated(2),
+    piece.getRotated(3),
+  ]
+}, [])
+
+interface IPieceHolder {
+  piece: Piece
+  x: number
+  y: number
+}
+
+class Map {
+  pieceHolders: IPieceHolder[]
+
+  constructor() {
+    this.pieceHolders = []
+
+    for (let x = 0; x < 20; x++) {
+      for (let y = 0; y < 10; y++) {
+        for (let i = 0; i < 1000; i++) {
+          const randomPiece = allRotatedPieces[Math.floor(Math.random() * allRotatedPieces.length)]
+          if (this.pieceOkHere(x, y, randomPiece)) {
+            this.pieceHolders.push({
+              x, y, piece: randomPiece,
+            })
+            break
+          }
+        }
+      }
+    }
+  }
+
+  getAt(x: number, y: number) {
+    return this.pieceHolders.filter(p => p.x === x && p.y === y)[0]
+  }
+
+  pieceOkHere(x: number, y: number, piece: Piece) {
+    const left = this.getAt(x - 1, y)
+    const right = this.getAt(x + 1, y)
+    const top = this.getAt(x, y - 1)
+    const bottom = this.getAt(x, y + 1)
+
+    if (left && left.piece.getRight() !== piece.getLeft()) {
+      return false
+    }
+    if (right && right.piece.getLeft() !== piece.getLeft()) {
+      return false
+    }
+    if (bottom && bottom.piece.getTop() !== piece.getBottom()) {
+      return false
+    }
+    if (top && top.piece.getBottom() !== piece.getTop()) {
+      return false
+    }
+
+    return true
+  }
+
+  getRange() {
+    return {
+      x: {
+        min: Math.min(...this.pieceHolders.map(p => p.x)),
+        max: Math.max(...this.pieceHolders.map(p => p.x)),
+      },
+      y: {
+        min: Math.min(...this.pieceHolders.map(p => p.y)),
+        max: Math.max(...this.pieceHolders.map(p => p.y)),
+      }
+    }
+  }
+}
+
+const MapDisplay = ({map}: { map: Map }) => {
+  const mapRange = React.useMemo(() => map.getRange(), [])
+
+  return <div className={'map-display'}>
+    <table>
+      {range(mapRange.y.min, mapRange.y.max).map(y => {
+        return <tr>
+          {range(mapRange.x.min, mapRange.x.max).map(x => {
+            const piece = map.getAt(x, y)
+            if (!piece) {
+              return <td></td>
+            }
+            return <td>
+              <img
+                src={piece.piece.getImageDataUrl()}
+                onClick={(event) => {
+                  console.log('clicked', piece.x, piece.y, event.movementX, event.movementY)
+                  debugger
+                }}
+              />
+            </td>
+          })}
+        </tr>
+      })}
+    </table>
+  </div>
+}
+
+const App = () => {
+  const [map, setMap] = React.useState(new Map())
+
+  return (
+    <div>
+      <MapDisplay map={map}/>
+      <div className="outer">
+        {pieces.map(piece => {
+          return <div className={"inner"}>
+            {[0, 1, 2, 3].map(rotation => {
+              return <img src={piece.getRotated(rotation).getImageDataUrl()}/>
+            })}
+          </div>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
