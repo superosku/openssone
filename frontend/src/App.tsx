0f40e283608effd0829ee0aacb4a1988d731a75f
Oskari Lehto <oskari.lehto@gmail.com>
2021-09-10 17:49:40 +0300

    Refactor page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,24 @@ import {GameMap} from "./game/GameMap";
 import {axiosInstance, getImageDataUrl} from "./utils";
 import {MapDisplay} from "./components/MapDisplay";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 const getRandomPiece = () => {
   return pieces[Math.floor(Math.random() * pieces.length)]
 }
 
 interface IGameProps {
-  zoomLevel: number | undefined
+  // zoomLevel: number | undefined
 }
 
-const Game = ({zoomLevel}: IGameProps) => {
+const Game = ({}: IGameProps) => {
+  const [zoomLevel, setZoomLevel] = React.useState(100)
   const [map, setMap] = React.useState(new GameMap())
   const [nextPiece, setNextPiece] = React.useState(getRandomPiece())
   const [nextPieceRotation, setNextPieceRotation] = React.useState(0)
@@ -26,7 +34,7 @@ const Game = ({zoomLevel}: IGameProps) => {
     setMap(newMap)
   }, [])
 
-  return <div className={'game'}>
+  return <div className={'game main-limited'}>
     <div className={'map-container'}>
       <MapDisplay
         zoomLevel={zoomLevel}
@@ -74,11 +82,8 @@ const Game = ({zoomLevel}: IGameProps) => {
   </div>
 }
 
-const App = () => {
+const GeneratedMap = () => {
   const [map, setMap] = React.useState(new GameMap())
-  const [showDebug, setShowDebug] = React.useState(true)
-  const [zoomLevel, setZoomLevel] = React.useState<undefined | number>(undefined)
-  const [menuOpen, setMenuOpen] = React.useState(false)
 
   React.useEffect(() => {
     let newMap = map.clone()
@@ -86,106 +91,74 @@ const App = () => {
     setMap(newMap)
   }, [])
 
-  React.useEffect(() => {
-    console.log('Opening socket')
+  return <div className={'main-limited'}>
+    <MapDisplay map={map}/>
+  </div>
+}
 
-    const socket = new WebSocket('ws://localhost:8888/messages');
-
-    socket.addEventListener('close', (event) => {
-      console.log('Socket closed')
-    });
-
-    socket.addEventListener('open', (event) => {
-      console.log('Socket opened')
-      socket.send('Hello Server!');
-    });
-
-    socket.addEventListener('message', (event) => {
-      console.log('Message from server ', event.data);
-    });
-  }, [])
-
-  return (
-    <div className={'main-container'}>
-      <div>
-        <button
-          onClick={async () => {
-            const response = await axiosInstance.post('/games/new')
-            console.log(response)
-          }}
-        >newGame
-        </button>
-        <button
-          onClick={async () => {
-            const response = await axiosInstance.post('/games/join/0ekfi')
-            console.log(response)
-          }}>joinGame
-        </button>
-        <button
-          onClick={async () => {
-            const response = await axiosInstance.get('/games/613aefce6ef6423566001988')
-            console.log(response)
-          }}>getGame
-        </button>
-        <button
-          onClick={async () => {
-            const response = await axiosInstance.post(
-              '/games/613aefce6ef6423566001988/pieces',
-              {
-                x: Math.floor(Math.random() * 10) - 5,
-                y: Math.floor(Math.random() * 10) - 5,
-                piece: allRotatedPieces[
-                  Math.floor(Math.random() * allRotatedPieces.length)
-                  ].asJson()
-              }
-            )
-            console.log(response)
-          }}>postPiece
-        </button>
-      </div>
-      <div className={'menu' + (menuOpen ? ' open' : ' closed')}>
-        {menuOpen ? <>
-          <FaTimes className={'times'} onClick={() => {
-            setMenuOpen(!menuOpen)
-          }}/>
-          <span onClick={() => {
-            setShowDebug(!showDebug)
-          }}>Toggle debug</span>
-          <span onClick={() => {
-            setZoomLevel(undefined)
-          }}>100%</span>
-          <span onClick={() => {
-            setZoomLevel(75)
-          }}>75%</span>
-          <span onClick={() => {
-            setZoomLevel(50)
-          }}>50%</span>
-          <span onClick={() => {
-            setZoomLevel(25)
-          }}>25%</span>
-        </> : <FaBars className={'bars'} onClick={() => {
-          setMenuOpen(!menuOpen)
-        }}/>}
-      </div>
-      {!showDebug && <Game zoomLevel={zoomLevel}/>}
-      {showDebug &&
-      <div>
-        <h1>Generated map</h1>
-        <MapDisplay map={map}/>
-        <h1>All pieces</h1>
-        <div className="outer">
-          {pieces.map(piece => {
-            return <div className={"inner"} key={piece.getHash()}>
-              {[0, 1, 2, 3].map(rotation => {
-                return <img key={rotation} src={getImageDataUrl(piece.getRotated(rotation))}/>
-              })}
-            </div>
+const AllPieces = () => {
+  return <div className={'main-limited'}>
+    <div className="outer">
+      {pieces.map(piece => {
+        return <div className={"inner"} key={piece.getHash()}>
+          {[0, 1, 2, 3].map(rotation => {
+            return <img key={rotation} src={getImageDataUrl(piece.getRotated(rotation))}/>
           })}
         </div>
-      </div>
-      }
+      })}
     </div>
-  );
+  </div>
+}
+
+const App = () => {
+  // const [map, setMap] = React.useState(new GameMap())
+  // const [showDebug, setShowDebug] = React.useState(false)
+  // const [zoomLevel, setZoomLevel] = React.useState<undefined | number>(undefined)
+  // const [menuOpen, setMenuOpen] = React.useState(false)
+  //
+  // React.useEffect(() => {
+  //   let newMap = map.clone()
+  //   newMap.randomize()
+  //   setMap(newMap)
+  // }, [])
+
+  // React.useEffect(() => {
+  //   console.log('Opening socket')
+  //
+  //   const socket = new WebSocket('ws://localhost:8888/messages');
+  //
+  //   socket.addEventListener('close', (event) => {
+  //     console.log('Socket closed')
+  //   });
+  //
+  //   socket.addEventListener('open', (event) => {
+  //     console.log('Socket opened')
+  //     socket.send('Hello Server!');
+  //   });
+  //
+  //   socket.addEventListener('message', (event) => {
+  //     console.log('Message from server ', event.data);
+  //   });
+  // }, [])
+
+  return <Router>
+    <div className={'main-container'}>
+      <nav>
+        <ul>
+          <li><Link to={'/'}>Try</Link></li>
+          <li><Link to={'/generated'}>Generated</Link></li>
+          <li><Link to={'/pieces'}>Pieces</Link></li>
+          <li><Link to={'/games'}>Games</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path={'/'} exact><Game/></Route>
+        <Route path={'/generated'}><GeneratedMap/></Route>
+        <Route path={'/pieces'}><AllPieces/></Route>
+        <Route path={'/games'}></Route>
+      </Switch>
+    </div>
+  </Router>
 }
 
 export default App;
