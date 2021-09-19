0a65150514901635c66a37641b5b85fae0b522a7
Oskari Lehto <oskari.lehto@gmail.com>
2021-09-19 15:21:53 +0300

    Update game list visuals

--- frontend/src/layouts/Games.tsx
+++ frontend/src/layouts/Games.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import './Games.scss';
 import {useHistory} from "react-router-dom";
 import {IGameInfo, IGameState} from "common";
+import {FaRedo} from 'react-icons/fa';
+import {GiMeeple} from 'react-icons/gi';
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 import {Loader} from "../components/Loader";
-import {axiosInstance} from "../utils";
+import {axiosInstance, getRange} from "../utils";
+
+dayjs.extend(relativeTime);
 
 export const Games = (): JSX.Element => {
   const [games, setGames] = React.useState<IGameState[]>([]);
@@ -11,6 +18,8 @@ export const Games = (): JSX.Element => {
   const history = useHistory();
 
   const loadGames = async () => {
+    setLoading(true);
+    setGames([]);
     const response = await axiosInstance.get('/games');
     setGames(response.data);
     setLoading(false);
@@ -20,43 +29,65 @@ export const Games = (): JSX.Element => {
     loadGames();
   }, []);
 
-  return <div>
-    <ul>
+  if (loading) {
+    return <div className={'games'}>
+      <Loader/>
+    </div>;
+  }
+
+  return <div className={'games'}>
+    <div className={'top-controls'}>
+      <FaRedo
+        className={'refresh'}
+        onClick={loadGames}
+      />
+    </div>
+    <ul className={'game-list'}>
       {games.map((game) => {
-        return <li key={game.id}>
-          {game.id} -
-          {game.createdAt} -
-          {game.joinSlug}
-          {game.status === 'created' &&
-          <button
-            onClick={async () => {
-              const response = await axiosInstance.post(`/games/join/${game.joinSlug}`);
-              const gameId = response.data.data._id;
-              const joinSlug = response.data.meta.you.joinSlug;
-              history.push(`/games/${gameId}/${joinSlug}`);
-            }}>Join
-          </button>
-          }
+        return <li className={'game'} key={game.id}>
+          <div>
+            <div>
+              Created: <span className={'created'}>{dayjs(game.createdAt).fromNow()}</span>
+            </div>
+            <div>
+              Players: <span>{getRange(0, game.players.length).map((i) => {
+                return <GiMeeple
+                key={i}
+                className={`player-${i}`}
+              />;
+              })}</span>
+            </div>
+          </div>
+          <div className={'right-part'}>
+            <div>
+              <span className={`status ${game.status}`}>{game.status}</span>
+            </div>
+            {game.status === 'created' && <button
+              className={'btn'}
+              onClick={async () => {
+                const response = await axiosInstance.post(`/games/join/${game.joinSlug}`);
+                const gameId = response.data.data._id;
+                const joinSlug = response.data.meta.you.joinSlug;
+                history.push(`/games/${gameId}/${joinSlug}`);
+              }}>Join
+            </button>}
+          </div>
         </li>;
       })}
     </ul>
-    {loading ? <Loader/>
-      : <div>
-        <button onClick={loadGames}>
-          Refresh
-        </button>
-        <button
-          onClick={async () => {
-            setLoading(true);
-            const response = await axiosInstance.post('/games/new');
-            const responseGame: IGameInfo = response.data;
-            const gameId = responseGame.data.id;
-            const joinSlug = responseGame.meta.you.joinSlug;
-            history.push(`/games/${gameId}/${joinSlug}`);
-          }}
-        >newGame
-        </button>
-      </div>
-    }
+    <div className={'bottom-controls'}>
+      <button
+        className={'btn'}
+        onClick={async () => {
+          setLoading(true);
+          const response = await axiosInstance.post('/games/new');
+          const responseGame: IGameInfo = response.data;
+          const gameId = responseGame.data.id;
+          const joinSlug = responseGame.meta.you.joinSlug;
+          history.push(`/games/${gameId}/${joinSlug}`);
+        }}
+      >New game
+      </button>
+    </div>
   </div>;
 };
